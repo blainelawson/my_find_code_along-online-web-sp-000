1e490f05ed9acaa7cbefebe115144081a81ee52b
Blaine Lawson <blaine.lawson@gmail.com>
2019-03-11 22:00:10 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,11 +4,10 @@ def my_find(collection)
   i = 0
 
   while i < collection.length
-    if yield(collection[i])
-      return collectionp[i]
-    end
+    return collectionp[i] if yield(collection[i])   
     i += 1
   end
 end
 
+collection = (1..100).to_a
 my_find(collection) {|i| i % 3 == 0 && i % 5 == 0}
